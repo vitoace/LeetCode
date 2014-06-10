da2c23b83769ceda5e24ba61fdad42d2035b8d8b
Vito <vitoace@gmail.com>
2014-06-09 21:03:50 -0400

    Single Number II solved with bit operation

---
+++ SingleNumberII/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  return 0;
+}
+
+int singleNumber(int A[], int n) {
+  int ones=0, twos=0, xthrees=0;
+  for (int i = 0; i < n; i++) {
+    twos |= (ones & A[i]);
+    ones ^= ones & A[i];
+    xthrees = ~(ones & twos);
+    ones &= xthrees;
+    twos &= xthrees;
+  }
+  return ones;
+}
