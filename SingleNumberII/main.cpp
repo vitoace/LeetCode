adb95296dfcde28aa86502c52e8f61770da00546
Vito <vitoace@gmail.com>
2014-06-09 21:14:31 -0400

    fixed minor bug

--- SingleNumberII/main.cpp
+++ SingleNumberII/main.cpp
@@ -10,7 +10,7 @@ int singleNumber(int A[], int n) {
   int ones=0, twos=0, xthrees=0;
   for (int i = 0; i < n; i++) {
     twos |= (ones & A[i]);
-    ones ^= ones & A[i];
+    ones ^= A[i];
     xthrees = ~(ones & twos);
     ones &= xthrees;
     twos &= xthrees;
