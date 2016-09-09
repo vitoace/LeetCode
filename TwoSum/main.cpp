e95e4b2d1f541b46d8a4bc89ae042ed9e6ff1bcb
Vito Wang <vitoace@gmail.com>
2016-09-08 17:31:44 -0700

    O(nlog(n)) method implemented

--- TwoSum/main.cpp
+++ TwoSum/main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
+vector<int> twoSum2(vector<int>& nums, int target);
 vector<int> twoSum(vector<int>& nums, int target);
 int main() {
-  vector<int> nums{2, 7, 10, 11};
-  int target = 9;
-  vector<int> res = twoSum(nums, target);
+  vector<int> nums{3,2,4};
+  int target = 6;
+  vector<int> res = twoSum2(nums, target);
   for (int i = 0; i < res.size(); ++i)
     cout << res[i] << ' ';
   cout << endl;
@@ -24,3 +26,20 @@ vector<int> twoSum(vector<int>& nums, int target) {
     }      
   }
 }
+
+vector<int> twoSum2(vector<int>& nums, int target) {
+  map<int, int> temp;
+  vector<int> res;
+  for (int i = 0; i < nums.size(); ++i)
+    temp[nums[i]] = i;
+  for (int i = 0; i < nums.size(); ++i)
+    
+    if (temp.find(target - nums[i]) != temp.end()
+	&& temp[target-nums[i]] != i) {
+      res.push_back(i);
+      res.push_back(temp[target-nums[i]]);
+      return res;
+    }
+  return res;
+}
+
